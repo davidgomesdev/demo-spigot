339a31c3002466486ae528522a9e36afed135a35
davidgomesdev <10091092+davidgomesdev@users.noreply.github.com>
2026-02-12 22:32:51 +0000

    feat: added arena manager

--- src/main/kotlin/me/davidgomes/demo/Main.kt
+++ src/main/kotlin/me/davidgomes/demo/Main.kt
@@ -1,13 +1,19 @@
 package me.davidgomes.demo
 
+import me.davidgomes.demo.arena.ArenaEventHandler
+import me.davidgomes.demo.arena.ArenaManager
 import me.davidgomes.demo.heroes.butcher.AnvilDropEventHandler
 import org.bukkit.plugin.java.JavaPlugin
 
 @Suppress("unused")
 object Main : JavaPlugin() {
     override fun onEnable() {
-        logger.info("Enabled")
+        val arenaManager = ArenaManager(logger)
+
         server.pluginManager.registerEvents(AnvilDropEventHandler(this, logger), this)
+        server.pluginManager.registerEvents(ArenaEventHandler(arenaManager), this)
+
+        logger.info("Enabled")
     }
 
     override fun onDisable() {
