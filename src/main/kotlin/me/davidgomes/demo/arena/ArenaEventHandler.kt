082963dc287a5a3bc359ac5dc7bdf6645633b875
davidgomesdev <10091092+davidgomesdev@users.noreply.github.com>
2026-02-14 11:00:53 +0000

    test: add tests for ArenaManager

--- src/main/kotlin/me/davidgomes/demo/arena/ArenaEventHandler.kt
+++ src/main/kotlin/me/davidgomes/demo/arena/ArenaEventHandler.kt
@@ -4,6 +4,7 @@ import net.kyori.adventure.text.Component
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
@@ -13,9 +14,7 @@ import org.bukkit.inventory.ItemStack
 class ArenaEventHandler(val arenaManager: ArenaManager) : Listener {
 
     val arenaJoinItem = ItemStack(Material.CLOCK).apply {
-        itemMeta.apply {
-            displayName(Component.text("Join Arena"))
-        }
+        editMeta { it.customName(Component.text("Join Arena")) }
     }
 
     @EventHandler
@@ -33,6 +32,8 @@ class ArenaEventHandler(val arenaManager: ArenaManager) : Listener {
     @EventHandler
     fun onPlayerDropItem(event: PlayerDropItemEvent) {
         val item = event.itemDrop.itemStack
+
+        // TODO: not working
         if (item.type == arenaJoinItem.type && item.itemMeta?.customName() == arenaJoinItem.displayName()) {
             event.isCancelled = true
         }
@@ -40,12 +41,21 @@ class ArenaEventHandler(val arenaManager: ArenaManager) : Listener {
 
     @EventHandler
     fun onPlayerInteract(event: PlayerInteractEvent) {
+        if (event.action != Action.RIGHT_CLICK_AIR && event.action != Action.RIGHT_CLICK_BLOCK) return
+
+        if (arenaManager.isInArena(event.player.uniqueId)) {
+            event.player.sendMessage(Component.text("You are already in the arena!"))
+
+            return
+        }
+
         val item = event.item ?: return
 
         if (!(item.type == arenaJoinItem.type && item.itemMeta?.customName() == arenaJoinItem.displayName())) return
 
         arenaManager.joinArena(event.player.uniqueId)
         event.player.sendMessage(Component.text("You have joined the arena!"))
+
         event.isCancelled = true
     }
 }
