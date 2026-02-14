6010368208c6aff281b2e243c28426e14588b45e
davidgomesdev <10091092+davidgomesdev@users.noreply.github.com>
2026-02-14 15:40:58 +0000

    test: add tests for ArenaEventHandler

--- src/main/kotlin/me/davidgomes/demo/arena/ArenaEventHandler.kt
+++ src/main/kotlin/me/davidgomes/demo/arena/ArenaEventHandler.kt
@@ -1,6 +1,7 @@
 package me.davidgomes.demo.arena
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextComponent
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -11,10 +12,14 @@ import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.inventory.ItemStack
 
+// TODO: remove arena join item when player joins arena, and give it back when they leave
 class ArenaEventHandler(val arenaManager: ArenaManager) : Listener {
 
+    val joinItemName = Component.text("Join Arena")
+
+    // TODO: make a class for interactable items
     val arenaJoinItem = ItemStack(Material.CLOCK).apply {
-        editMeta { it.customName(Component.text("Join Arena")) }
+        editMeta { it.customName(joinItemName) }
     }
 
     @EventHandler
@@ -26,36 +31,42 @@ class ArenaEventHandler(val arenaManager: ArenaManager) : Listener {
 
     @EventHandler
     fun onPlayerQuit(event: PlayerQuitEvent) {
-        arenaManager.leaveArena(event.player.uniqueId)
+        if (arenaManager.isInArena(event.player.uniqueId)) {
+            arenaManager.leaveArena(event.player.uniqueId)
+        }
     }
 
     @EventHandler
     fun onPlayerDropItem(event: PlayerDropItemEvent) {
         val item = event.itemDrop.itemStack
 
-        // TODO: not working
-        if (item.type == arenaJoinItem.type && item.itemMeta?.customName() == arenaJoinItem.displayName()) {
-            event.isCancelled = true
-        }
+        val customName = item.itemMeta?.customName() ?: return
+        if (customName !is TextComponent) return
+
+        if (!(item.type == arenaJoinItem.type && customName == joinItemName)) return
+
+        event.isCancelled = true
     }
 
     @EventHandler
     fun onPlayerInteract(event: PlayerInteractEvent) {
         if (event.action != Action.RIGHT_CLICK_AIR && event.action != Action.RIGHT_CLICK_BLOCK) return
 
+        val item = event.item ?: return
+
+        val customName = item.itemMeta?.customName() ?: return
+        if (customName !is TextComponent) return
+
+        if (!(item.type == arenaJoinItem.type && customName == joinItemName)) return
+
+        event.isCancelled = true
+
         if (arenaManager.isInArena(event.player.uniqueId)) {
             event.player.sendMessage(Component.text("You are already in the arena!"))
-
             return
         }
 
-        val item = event.item ?: return
-
-        if (!(item.type == arenaJoinItem.type && item.itemMeta?.customName() == arenaJoinItem.displayName())) return
-
         arenaManager.joinArena(event.player.uniqueId)
         event.player.sendMessage(Component.text("You have joined the arena!"))
-
-        event.isCancelled = true
     }
 }
